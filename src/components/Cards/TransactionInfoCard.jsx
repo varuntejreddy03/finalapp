cbd74e5eb145f39165c520d3cab1394bdc149b8e
varuntejreddy03 <152944384+varuntejreddy03@users.noreply.github.com>
2025-09-20 01:39:31 +0530

    Update TransactionInfoCard.jsx

--- src/components/Cards/TransactionInfoCard.jsx
+++ src/components/Cards/TransactionInfoCard.jsx
@@ -21,11 +21,7 @@ const TransactionInfoCard = ({
     return (
         <div className="group relative flex items-center gap-4 mt-2 p-3 rounded-lg hover:bg-gray-100/60">
             <div className="w-12 h-12 flex items-center justify-center text-xl text-gray-800 bg-gray-100 rounded-full">
-                {icon ? (
-                    <span className="text-2xl">{icon}</span>
-                ) : (
-                    <LuUtensils />
-                )}
+                <LuUtensils />
             </div>
 
             <div className="flex-1 flex items-center justify-between">
